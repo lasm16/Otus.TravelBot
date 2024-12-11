e3cdc4fc227ce94681bcfcdb70ca5ca5dbb0027c
lasm16 <lasm16@yandex.ru>
2024-12-12 00:49:02 +0300

    Add inline keyboard sample

--- Common/Model/Bot/IScenario.cs
+++ Common/Model/Bot/IScenario.cs
@@ -1,7 +1,10 @@
-﻿namespace Common.Model.Bot
+﻿using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Common.Model.Bot
 {
     public interface IScenario
     {
+        public InlineKeyboardMarkup InlineKeyboard { get; set; }
         string? Text { get; set; }
         void DoAction();
     }
--- TelegramBot/Business/Scenarios/GreetingsScenario.cs
+++ TelegramBot/Business/Scenarios/GreetingsScenario.cs
@@ -1,5 +1,6 @@
 ﻿using Common.Model.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBot.Business.Scenarios
 {
@@ -8,11 +9,19 @@ namespace TelegramBot.Business.Scenarios
         private Message _message = message;
 
         public string? Text { get; set; }
+        public InlineKeyboardMarkup InlineKeyboard { get; set; }
 
         public void DoAction()
         {
             var currentUser = _message.From!.FirstName + " " + _message.From.LastName;
-            Text = $"Приветствую тебя, {currentUser}! Ты можешь выложить пост о планируемой поездке или найти попутчика";
+            Text = $"Приветствую тебя, {currentUser}! Ты можешь выложить пост о планируемой поездке или найти попутчика.";
+
+
+            var inlineMarkup = new InlineKeyboardMarkup()
+                .AddButton("Создать новую поездку", "CreateNewTripScenario")
+                .AddButton("Найти попутчика", "FindFellowScenario");
+
+            InlineKeyboard = inlineMarkup;
         }
     }
 }
