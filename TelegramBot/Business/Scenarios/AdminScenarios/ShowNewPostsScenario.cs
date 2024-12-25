296678dd48c3c4e1e60ef9052261cc4557e9bd61
Alex Lyubimov <lasm16@yandex.ru>
2024-12-25 18:16:40 +0400

    Complete show new post scenario logic

--- TelegramBot/Business/Scenarios/AdminScenarios/ShowNewPostsScenario.cs
+++ TelegramBot/Business/Scenarios/AdminScenarios/ShowNewPostsScenario.cs
@@ -8,12 +8,14 @@ using Common.Model;
 using Common.Data;
 using TelegramBot.Business.Bot;
 using System.Text;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBot.Business.Scenarios.AdminScenarios
 {
     public class ShowNewPostsScenario(TelegramBotClient botClient) : IScenario
     {
         private object? _currentTrip;
+        private List<object> _trips = GetNewTripsWithUserName();
         private static List<Post> _posts = Repository.Posts;
         private TelegramBotClient _botClient = botClient;
 
@@ -39,42 +41,123 @@ namespace TelegramBot.Business.Scenarios.AdminScenarios
             if (update.CallbackQuery.Data.Equals("Принять все"))
             {
                 var chatId = update.CallbackQuery!.Message!.Chat.Id;
+                var messageId = update.CallbackQuery.Message.Id;
                 var newTrips = GetNewTrips();
                 newTrips.ForEach(x => x.Status = TripStatus.Accepted);
                 //сохранить в БД
+                await _botClient.EditMessageReplyMarkup(chatId, messageId, replyMarkup: null);
                 await _botClient.SendMessage(chatId, BotPhrases.AllTripsAccepted);
             }
-            if (update.CallbackQuery.Data.Equals("Отклоноить все"))
+            if (update.CallbackQuery.Data.Equals("Отклонить все"))
             {
                 var chatId = update.CallbackQuery!.Message!.Chat.Id;
+                var messageId = update.CallbackQuery.Message.Id;
                 var newTrips = GetNewTrips();
                 newTrips.ForEach(x => x.Status = TripStatus.Declined);
                 //сохранить в БД
+                await _botClient.EditMessageReplyMarkup(chatId, messageId, replyMarkup: null);
                 await _botClient.SendMessage(chatId, BotPhrases.AllTripsDeclined);
             }
             if (update.CallbackQuery.Data.Equals("Посмотреть"))
             {
                 var chatId = update.CallbackQuery!.Message!.Chat.Id;
                 var messageId = update.CallbackQuery.Message.Id;
-                var trips = GetNewTripsWithUserName();
-                if (trips.Count == 0)
+                if (_trips.Count == 0)
                 {
                     await _botClient.SendMessage(chatId, BotPhrases.TripsNotFound);
                     return;
                 }
-                await _botClient.DeleteMessage(chatId, messageId);
-                var tripObject = trips[0];
-                _currentTrip = tripObject;
-                var (text, photo) = GetTripText(tripObject);
+                var trip = _trips[0];
+                _currentTrip = trip;
+                var (text, photo) = GetTripText(trip);
 
                 var inlineMarkup = TelegramBotImpl.GetInlineKeyboardMarkup("Принять", "Отклонить");
 
-                if (trips.Count > 1)
+                if (_trips.Count > 1)
                 {
                     inlineMarkup = TelegramBotImpl.GetInlineKeyboardMarkup("Принять", "Отклонить", "Далее");
                 }
+                await _botClient.EditMessageReplyMarkup(chatId, messageId, replyMarkup: null);
                 await _botClient.SendPhoto(chatId, photo, text, replyMarkup: inlineMarkup);
             }
+            if (update.CallbackQuery.Data.Equals("Далее"))
+            {
+                var chatId = update.CallbackQuery!.Message!.Chat.Id;
+                var messageId = update.CallbackQuery.Message.Id;
+                var index = _trips.IndexOf(_currentTrip) + 1;
+                var trip = _trips[index];
+                _currentTrip = trip;
+                var (text, photo) = GetTripText(trip);
+                var inlineMarkup = TelegramBotImpl.GetInlineKeyboardMarkup("Принять", "Отклонить", "Назад", "Далее");
+                if (index == _trips.Count - 1)
+                {
+                    inlineMarkup = TelegramBotImpl.GetInlineKeyboardMarkup("Принять", "Отклонить", "Назад");
+                }
+                await _botClient.DeleteMessage(chatId, messageId);
+                await _botClient.SendPhoto(chatId, photo, text, replyMarkup: inlineMarkup);
+            }
+            if (update.CallbackQuery.Data.Equals("Назад"))
+            {
+                var chatId = update.CallbackQuery!.Message!.Chat.Id;
+                var messageId = update.CallbackQuery.Message.Id;
+                var index = _trips.IndexOf(_currentTrip) - 1;
+                var trip = _trips[index];
+                _currentTrip = trip;
+                var (text, photo) = GetTripText(trip);
+                var inlineMarkup = TelegramBotImpl.GetInlineKeyboardMarkup("Принять", "Отклонить", "Назад", "Далее");
+                if (index == 0)
+                {
+                    inlineMarkup = TelegramBotImpl.GetInlineKeyboardMarkup("Принять", "Отклонить", "Далее");
+                }
+                await _botClient.DeleteMessage(chatId, messageId);
+                await _botClient.SendPhoto(chatId, photo, text, replyMarkup: inlineMarkup);
+            }
+            if (update.CallbackQuery.Data.Equals("Принять") || update.CallbackQuery.Data.Equals("Отклонить"))
+            {
+                var chatId = update.CallbackQuery!.Message!.Chat.Id;
+                var messageId = update.CallbackQuery.Message.Id;
+                var tripToAccept = _currentTrip;
+                //Сохранение данных в БД
+                var index = _trips.IndexOf(tripToAccept);
+                _trips.Remove(tripToAccept);
+                await _botClient.DeleteMessage(chatId, messageId);
+                if (_trips.Count == 0)
+                {
+                    await _botClient.SendMessage(chatId, BotPhrases.TripsNotFound);
+                    return;
+                }
+                if (_trips.Count == index)
+                {
+                    index--;
+                }
+                var trip = _trips[index];
+                _currentTrip = trip;
+                var (text, photo) = GetTripText(trip);
+                var inlineMarkup = GetNavigationButtons(_trips.Count, index);
+                await _botClient.SendPhoto(chatId, photo, text, replyMarkup: inlineMarkup);
+            }
+        }
+
+        private static InlineKeyboardMarkup? GetNavigationButtons(int count, int index)
+        {
+            InlineKeyboardMarkup? inlineMarkup = null;
+            if (count == 1)
+            {
+                inlineMarkup = TelegramBotImpl.GetInlineKeyboardMarkup("Принять", "Отклонить");
+            }
+            if (count > 1 && index == 0)
+            {
+                inlineMarkup = TelegramBotImpl.GetInlineKeyboardMarkup("Принять", "Отклонить", "Далее");
+            }
+            if (count > 1 && index != 0)
+            {
+                inlineMarkup = TelegramBotImpl.GetInlineKeyboardMarkup("Принять", "Отклонить", "Назад", "Далее");
+            }
+            if (count > 1 && index == count - 1)
+            {
+                inlineMarkup = TelegramBotImpl.GetInlineKeyboardMarkup("Принять", "Отклонить", "Назад");
+            }
+            return inlineMarkup;
         }
 
         private static (string text, string photo) GetTripText(object tripObject)
@@ -138,7 +221,20 @@ namespace TelegramBot.Business.Scenarios.AdminScenarios
 
         private async Task OnMessage(Message message, UpdateType type)
         {
-            throw new NotImplementedException();
+            var inputLine = message.Text;
+            if (inputLine is null)
+            {
+                return;
+            }
+            if (!inputLine.Equals("/start"))
+            {
+                Log.Error("Некорректно указан сценарий!");
+                await _botClient.SendMessage(message.Chat.Id, BotPhrases.UnknownCommand);
+                return;
+            }
+            UnsubscribeEvents();
+            var scenario = new GreetingScenario(_botClient);
+            scenario.Launch();
         }
 
         private void UnsubscribeEvents()
